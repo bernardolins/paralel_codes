4e06d497280b94da9c692ebb235feb967114152d
Bernardo Lins <bernardolins28@gmail.com>
2015-03-27 09:26:24 -0300

    Add command line arguments and time counter to serial.

--- c2/serial.c
+++ c2/serial.c
@@ -1,6 +1,10 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include <math.h>
+#include <pthread.h>
+#include <unistd.h>
+#include <ctype.h>
+#include "timer.h"
 
 double pi_serial(int n) {
 	double result = 0.0;
@@ -14,8 +18,31 @@ double pi_serial(int n) {
 }
 
 
-int main(int argc, char const *argv[])
+int main(int argc, char *argv[])
 {
-	printf("%0.10lf\n", pi_serial(10000000));
+    char a;
+    int serie_length;
+    //getting arguments from command line
+    while((a = getopt(argc, argv, "n:")) != -1) {
+        switch(a) {
+        case 'n': serie_length = atoi(optarg); break;
+        }
+    }
+    
+     double time_ini;
+     GET_TIME(time_ini);
+
+
+    double pi = pi_serial(serie_length);
+    
+    double time_end;
+    GET_TIME(time_end);
+    
+
+    double elapsed_time = time_end - time_ini;
+	printf("%0.12lf\n", pi);
+
+
+    printf("serial elapsed time: %0.12lf\n", elapsed_time);
 	return 0;
 }
