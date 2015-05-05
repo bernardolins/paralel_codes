f2bd6c19e1cda1c1ed6226c8bfc92bcd90d435a2
Bernardo Lins <bernardolins28@gmail.com>
2015-05-05 20:49:13 -0300

    Add input parameters from terminal.

--- c3/paralel.c
+++ c3/paralel.c
@@ -1,1 +1,15 @@
 #include "timer.h"
+#include <stdio.h>
+#include <stdlib.h>
+
+int int main(int argc, char const *argv[])
+{
+	char a;
+	while((a = getopt(argc, argv, "s:")) != -1) {
+		switch(a) {
+		case 's': n_threads = atoi(optarg); break;
+		}
+	}
+
+	return 0;
+}
