1404fddb7ee7c194df5af11b4fad7f2f53afd757
Bernardo Lins <bernardolins28@gmail.com>
2015-05-06 00:26:20 -0300

    Implement paralel algorithm.

--- c3/paralel.c
+++ c3/paralel.c
@@ -4,23 +4,31 @@
 #include <stdlib.h>
 #include <unistd.h>
 #include <ctype.h>
+#include <pthread.h>
 
 int array_size = 0;
 int range = 0;
-int n_threads = 0; 
+int n_threads = 0;
+int* array; 
 
 int* results;
 
 void* tprefix_sum(void* arg) {
-	//pega 1/t do array
-	//soma normalmente
-	//pega o ultimo elemento e coloca no array results
+	int start_count = *(int*)arg;
+	int i;
+	int last = start_count + array_size/n_threads;
+	for(i = start_count + 1; i < last; i++) {
+		array[i] += array[i-1];
+	}
+
+	results[start_count/(array_size/n_threads)] = array[last];
+	return NULL;
 }
 
 int main(int argc, char *argv[])
 {
 	char a;
-	while((a = getopt(argc, argv, "s:r:")) != -1) {
+	while((a = getopt(argc, argv, "s:r:t:")) != -1) {
 		switch(a) {
 		case 's': array_size = atoi(optarg); break;
 		case 'r': range = atoi(optarg); break;
@@ -30,17 +38,22 @@ int main(int argc, char *argv[])
 
 	results = (int*)malloc(sizeof(int)*n_threads);
 
-	int* array = generate_random_array(array_size, range);
+	array = generate_random_array(array_size, range);
 
 	pthread_t* thread = (pthread_t*)malloc(sizeof(pthread_t)*n_threads);
 	int* start_count = (int*)malloc(sizeof(int)*n_threads);
 
+	print_generated(array, array_size);
+
+	int i;
 	for (i = 0; i < n_threads; i++) {
 		start_count[i] = i * array_size/n_threads;
 		if(pthread_create(&(thread[i]), NULL, &tprefix_sum, (void*)&start_count[i]) != 0) {
 			printf("Can't allocate thread\n");
 		}
 	}
+	printf("\n");
+	print_generated(array, array_size);
 
 	return 0;
 }
