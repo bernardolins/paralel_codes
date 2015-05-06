9b55a298202255d1f087cb3de7e94a3bf1ace025
Bernardo Lins <bernardolins28@gmail.com>
2015-05-05 23:55:28 -0300

    Add thread.

--- c3/paralel.c
+++ c3/paralel.c
@@ -7,6 +7,15 @@
 
 int array_size = 0;
 int range = 0;
+int n_threads = 0; 
+
+int* results;
+
+void* tprefix_sum(void* arg) {
+	//pega 1/t do array
+	//soma normalmente
+	//pega o ultimo elemento e coloca no array results
+}
 
 int main(int argc, char *argv[])
 {
@@ -15,10 +24,23 @@ int main(int argc, char *argv[])
 		switch(a) {
 		case 's': array_size = atoi(optarg); break;
 		case 'r': range = atoi(optarg); break;
+		case 't': n_threads = atoi(optarg); break;
 		}
 	}
 
+	results = (int*)malloc(sizeof(int)*n_threads);
+
 	int* array = generate_random_array(array_size, range);
+
+	pthread_t* thread = (pthread_t*)malloc(sizeof(pthread_t)*n_threads);
+	int* start_count = (int*)malloc(sizeof(int)*n_threads);
+
+	for (i = 0; i < n_threads; i++) {
+		start_count[i] = i * array_size/n_threads;
+		if(pthread_create(&(thread[i]), NULL, &tprefix_sum, (void*)&start_count[i]) != 0) {
+			printf("Can't allocate thread\n");
+		}
+	}
 
 	return 0;
 }
