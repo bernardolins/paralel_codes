17457a85eda94dcbac58db6b4c0344f5d6515b71
Bernardo Lins <bernardolins28@gmail.com>
2015-05-05 22:03:56 -0300

    Add range to generated arrays.

--- c3/generator.h
+++ c3/generator.h
@@ -1,11 +1,12 @@
 #include <time.h>
 #include <stdlib.h>
+#include <stdio.h>
 
-int* generate_random_array(int size) {
+int* generate_random_array(int size, int range) {
 	int* random_array = (int*)malloc(sizeof(int)*size);
 	int i;
 	for(i = 0; i < size; i++) {
-		random_array[i] = rand();
+		random_array[i] = (rand() % range);
 	}
 
 	return random_array;
