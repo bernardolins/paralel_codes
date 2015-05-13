f285957dcda2446448664c91610afcdd318ca397
Bernardo Lins <bernardolins28@gmail.com>
2015-05-13 09:11:37 -0300

    Create code for class 11.

---
+++ c11/pi_mpi.c
@@ -0,0 +1,22 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+double pi_serial(int n) {
+  double result = 0.0;
+  int i = 0;
+  for (i = 0; i < n; i++)
+  {
+    result += pow(-1.0, i)*(1.0/(2.0*i + 1));
+  }
+
+  return 4.0*result;
+}
+
+int main(int argc, char* argv[]) {
+    if(MPI_Init(&argc, &argv) != MPI_SUCCESS) {
+      printf("-- error while initializing MPI");
+    }
+
+    MPI_Finalize();
+    return 0;
+}
