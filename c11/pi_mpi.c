a99e43206df307d132904c53b936da47b6737d0c
Bernardo Lins <bernardolins28@gmail.com>
2015-05-13 09:28:31 -0300

    Add communication structure.

--- c11/pi_mpi.c
+++ c11/pi_mpi.c
@@ -3,23 +3,30 @@
 #include <math.h>
 #include "mpi.h"
 
-double pi_serial(int n) {
+double pi_serial(int ini, int end) {
   double result = 0.0;
-  int i = 0;
-  for (i = 0; i < n; i++)
+  int i = ini;
+  for (i = ini; i < end; i++)
   {
     result += pow(-1.0, i)*(1.0/(2.0*i + 1));
   }
 
-  return 4.0*result;
+  return result;
 }
 
 int main(int argc, char* argv[]) {
-    if(MPI_Init(&argc, &argv) == MPI_SUCCESS) {
+    int rank, nproc;
+    int pi = 0;
+    int serie_length = 0;
 
+    if(MPI_Init(&argc, &argv) == MPI_SUCCESS) {
+      MPI_Comm_rank(MPI_COMM_WORD, &rank);
+      MPI_Comm_size(MPI_COMM_WORD, &nproc);
     } else {
       printf("-- error while initializing MPI");
     }
+
+    int ini = rank*serie_length/nproc;
 
     MPI_Finalize();
     return 0;
